775700238c28d94e77568afbed0ac85b4c2e4598
Gabrieli <gabrielidelimamartins@gmail.com>
2025-11-07 14:55:43 -0300

    mudanças no menu

--- Menu.ts
+++ Menu.ts
@@ -7,26 +7,27 @@ export function main() {
 
     while (true){
         
-            console.log(colors.bg.black, colors.fg.yellow,
-                "************************************************************************************************");
-            console.log("                                                                                                ");
-            console.log("                                 BANCO DO BRAZIL COM Z                                          ");
-            console.log("                                                                                                ");
-            console.log("************************************************************************************************");
-            console.log("                                                                                                ");
-            console.log("                                   1 - Criar Conta                                              ");
-            console.log("                                   2 - Listar todas as Contas                                   ");
-            console.log("                                   3 - Buscar Conta por Numero                                  ");
-            console.log("                                   4 - Atualizar Dados da Conta                                 ");
-            console.log("                                   5 - Apagar Conta                                             ");
-            console.log("                                   6 - Sacar                                                    ");
-            console.log("                                   7 - Depositar                                                ");
-            console.log("                                   8 - Transferir valores entre Contas                          ");
-            console.log("                                   9 - Sair                                                     ");
-            console.log("                                                                                                ");
-            console.log("************************************************************************************************");
-            console.log("                                                                                                ",
-                colors.reset);
+        console.log(colors.bg.black, colors.fg.yellow);
+        console.log("********************************************************************");
+        console.log("                                                                    ");
+        console.log("                                                                    ");
+        console.log("                        BANCO DO BRAZIL COM Z                       ");
+        console.log("                                                                    ");
+        console.log("********************************************************************");
+        console.log("                                                                    ");
+        console.log("                       1 - Criar Conta                              ");
+        console.log("                       2 - Listar todas as Contas                   ");
+        console.log("                       3 - Buscar Conta por Numero                  ");
+        console.log("                       4 - Atualizar Dados da Conta                 ");
+        console.log("                       5 - Apagar Conta                             ");
+        console.log("                       6 - Sacar                                    ");
+        console.log("                       7 - Depositar                                ");
+        console.log("                       8 - Transferir valores entre Contas          ");
+        console.log("                       9 - Sair                                     ");
+        console.log("                                                                    ");
+        console.log("********************************************************************");
+        console.log("                                                                    ", 
+        colors.reset);
 
             console.log("Entre com a opcao desejada: ");
             opcao = readlinesync.questionInt("");
